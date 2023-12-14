2c05fca1700aa7fff669c03e9f35afd17199c97e
Adam Crockett <adam@duel.tech>
2023-12-14 15:29:20 +0000

    english language lib

---
+++ app/ui/src/lang.ts
@@ -0,0 +1,13 @@
+import { UIText } from "./types/Language.types";
+
+export const en: UIText = {
+  a11ySearchLabel: "Search",
+  addItem: "Add Item",
+  brandName: "Secure The Task",
+  defaultImageAltText: "profile image of a ninja chibi head",
+  loginPromptText: "Sign in",
+  reservedAnonymousUserName: "anonymous",
+  searchText: "search",
+  deleteBtnText: "delete",
+  editBtnText: "edit",
+};
