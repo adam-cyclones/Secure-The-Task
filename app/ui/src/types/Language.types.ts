ba9e5ff0265c5289771139c7ff89c813bc1f2055
Adam Crockett <adam@duel.tech>
2023-12-14 15:58:34 +0000

    defines the UI keys are regardless of the language

---
+++ app/ui/src/types/Language.types.ts
@@ -0,0 +1,14 @@
+/**
+ * Define the keys for all UI elements regaurdless of language
+ */
+export interface UIText {
+  a11ySearchLabel: string;
+  addItem: string;
+  brandName: "Secure The Task";
+  defaultImageAltText: string;
+  loginPromptText: string;
+  reservedAnonymousUserName: string;
+  searchText: string;
+  editBtnText: string;
+  deleteBtnText: string;
+}
