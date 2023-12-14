c3b33d8dce7615148389a350518682d347fd51f0
Adam Crockett <adam@duel.tech>
2023-12-14 15:27:21 +0000

    endpoints to impliment

---
+++ app/api-server/build/v1API.js
@@ -0,0 +1,19 @@
+import express from "express";
+const router = express.Router();
+// Placeholder function to send a "not implemented yet" response in JSON
+const notImplemented = (req, res) => {
+    res.status(501).json({ message: 'Not implemented yet' });
+};
+// Route to retrieve a list of tasks
+router.get('/tasks', notImplemented);
+// Route to create a new task
+router.post('/tasks', notImplemented);
+// Route to retrieve a task by its ID
+router.get('/tasks/:taskId', notImplemented);
+// Route to update an existing task
+router.put('/tasks/:taskId', notImplemented);
+// Route to delete a task
+router.delete('/tasks/:taskId', notImplemented);
+// Route to authenticate a user
+router.post('/authenticate', notImplemented);
+export default router;
