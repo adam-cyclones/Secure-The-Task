b023d3f2f531cd78efac3833e2c6e49f7d5021d4
Adam Crockett <adam@duel.tech>
2023-12-18 00:14:27 +0000

    formatting change

--- app/api-server/build/configuration.js
+++ app/api-server/build/configuration.js
@@ -1,6 +1,6 @@
 "use strict";
 module.exports = {
-    DEFAULT_HOSTNAME: process.env.HOSTNAME || "securetasklist.local",
-    DEFAULT_PORT: process.env.PORT || 3000,
-    DEFAULT_CERTS_BASENAME: "certs",
+  DEFAULT_HOSTNAME: process.env.HOSTNAME || "securetasklist.local",
+  DEFAULT_PORT: process.env.PORT || 3000,
+  DEFAULT_CERTS_BASENAME: "certs",
 };
