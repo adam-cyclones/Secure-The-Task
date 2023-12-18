5016104f57f20830e4e2a9bbd094fc73ade413e8
Adam Crockett <adam@duel.tech>
2023-12-18 00:26:39 +0000

    UI server mock is pretty slow, this text should gives a reaction to the action of loading

--- app/ui/src/components/TaskContent.tsx
+++ app/ui/src/components/TaskContent.tsx
@@ -1,6 +1,7 @@
 import { ListGroup } from "react-bootstrap";
 import type { Task } from "../types/App.types";
 import TaskCardItem from "./TaskCardItem";
+import { useUiText } from "../hooks/uiText";
 
 interface TaskContentProps {
   /**
@@ -10,10 +11,12 @@ interface TaskContentProps {
 }
 
 function TaskContent({ tasks }: TaskContentProps) {
+  const uiText = useUiText();
   return (
     <>
       <main>
         <ListGroup variant="flush">
+          {!tasks.length ? uiText.tasksLoadingMessage : ""}
           {tasks.map((task) => (
             <TaskCardItem key={task.id} task={task} />
           ))}
