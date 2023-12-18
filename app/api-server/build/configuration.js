dd4f7e13d9ff2e1e66d7ad99da8515525aad4890
Adam Crockett <adam@duel.tech>
2023-12-18 00:01:39 +0000

    in prod we need to change the hostname to localhost because not using mkcert in prod, overide port for EC2 internal

--- app/api-server/build/configuration.js
+++ app/api-server/build/configuration.js
@@ -1,5 +1,6 @@
-export default {
-  DEFAULT_HOSTNAME: "securetasklist.local",
-  DEFAULT_PORT: 3000,
-  DEFAULT_CERTS_BASENAME: "certs",
+"use strict";
+module.exports = {
+    DEFAULT_HOSTNAME: process.env.HOSTNAME || "securetasklist.local",
+    DEFAULT_PORT: process.env.PORT || 3000,
+    DEFAULT_CERTS_BASENAME: "certs",
 };
